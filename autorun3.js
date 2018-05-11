3183e8da89e423535b0b90b2eb1dd2748f53e2ce
ArisAgeha <87725092@qq.com>
2018-05-11 21:35:55 +0800

    about link modify

--- autorun3.js
+++ autorun3.js
@@ -7,7 +7,7 @@ function linkAssets() {
     $("<link>").attr({ href: "https://raw.githack.com/ArisAgeha/live2d/master/waifu.css", rel: "stylesheet", type: "text/css" }).appendTo('head');
     $('body').eq(0).append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" width="280" height="250" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $('body').eq(0).append($('<script src="https://raw.githack.com/ArisAgeha/live2d/master/live2d.js"></script>'));
-    $('body').eq(0).append($('<script src="https://raw.githack.com/ArisAgeha/live2d/master/waifu-tips.js"></script>'));
+    $('body').eq(0).append($('<script src="https://raw.githack.com/ArisAgeha/live2d/master/waifu-tips2.js"></script>'));
 }
 
 function clock(count) {
